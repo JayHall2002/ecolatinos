449a7044248f103b4a5726ffe149ee6b79c19a79
JayHall2002 <Ahallaj20002@gmail.com>
2024-10-09 18:41:32 -0500

    corrected global css and layout

--- website-app/app/page.js
+++ website-app/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold underline">Home Page</h1>
+    </div>
   );
 }
