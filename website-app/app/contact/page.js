6d41a94cdf30861eda8a70843d7366c4eebd7966
JayHall2002 <Ahallaj20002@gmail.com>
2024-10-09 20:18:48 -0500

    Made initial changes to Contact Us Page and Made functional button

--- website-app/app/contact/page.js
+++ website-app/app/contact/page.js
@@ -1,10 +1,24 @@
 import Navbar from "../components/navbar";
+import Link from "next/link";
 
 export default function Contact () {
     return(
-        <>
-          <h1>Contact</h1>
-          <Navbar />
-        </>
+        <div>
+            <Navbar />
+            <div className="text-4xl font-sans font-bold text-center text-blue-600">
+                <h1>Contact Us</h1>
+            </div>
+            <Link href="https://www.facebook.com/ecolatinos/" target="_blank" passHref>
+                <div className="container mx-auto text-center mt-7">
+                    <button className="items-center rounded-full 
+                    border-2 border-black-600 
+                    bg-green-600 px-4 py-2 text-white
+                    hover:scale-125 hover:bg-green-700"
+                    >
+                        Our Facebook
+                    </button>
+                </div>
+            </Link>
+        </div>
     );
 };
