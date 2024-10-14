2947f30396fa8ad706f9c67fda5621bf34d3b4ec
Cyrus <cbilpodi@umd.edu>
2024-10-14 14:47:16 -0400

    Added a header and footer to Impact page.

--- website-app/app/impact/page.js
+++ website-app/app/impact/page.js
@@ -1,11 +1,30 @@
-import Navbar from "../components/navbar";
+import Navbar from "../components/navbar.js";
 
 export default function Impact () {
     return(
         <>
-          
           <Navbar />
-          <h1>Impact</h1>
+          <Header />
+          <Footer />
         </>
     );
 };
+
+
+
+function Header() {
+  return (
+    <header>
+      <h1>Our Impact</h1>
+      <p>See how we're making a difference.</p>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Join us in making a difference!</p>
+    </footer>
+  );
+}
