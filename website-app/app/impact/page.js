9deb2dfc41e35d6f9c5ab2112e63a37693cbfc9e
cbil67 <166071758+cbil67@users.noreply.github.com>
2024-10-15 10:17:19 -0400

    Initial modifications for Impact page (#5)
    * Added a header and footer to Impact page.
    
    * Added a slight modification to the Impact heading.
    
    ---------
    
    Co-authored-by: Cyrus <cbilpodi@umd.edu>

--- website-app/app/impact/page.js
+++ website-app/app/impact/page.js
@@ -1,11 +1,30 @@
-import Navbar from "../components/navbar";
+import Navbar from "../components/navbar.js";
 
 export default function Impact () {
     return(
         <>
-          
           <Navbar />
-          <h1>Impact</h1>
+          <Header />
+          <Footer />
         </>
     );
 };
+
+
+
+function Header() {
+  return (
+    <header>
+      <h1 className="text-3xl font-sans text-center">Our Impact</h1>
+      <p>See how we're making a difference.</p>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Join us in making a difference!</p>
+    </footer>
+  );
+}
