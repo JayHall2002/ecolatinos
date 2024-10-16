dbc7d9e389ec01dcd265b6edd2cf765ce952b7de
Cyrus <cbilpodi@umd.edu>
2024-10-16 10:26:13 -0400

    Added header and footer specs.

--- website-app/app/impact/page.js
+++ website-app/app/impact/page.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "../components/navbar.js";
 
 export default function Impact () {
@@ -5,26 +6,49 @@ export default function Impact () {
         <>
           <Navbar />
           <Header />
+          <Paragraph1 />
           <Footer />
         </>
     );
 };
 
-
+function Paragraph1() {
+  return (
+    <p>
+      EcoLatinos has helped plant 54 trees in and around Saint Mary's of Landover's church, school 
+      and parking lot.
+    </p>
+  )
+}
 
 function Header() {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "Green",
+    padding: "10px",
+    fontFamily: "Sans-Serif",
+    textalign: "center"
+  };
+
   return (
     <header>
-      <h1 className="text-3xl font-sans text-center">Our Impact</h1>
+      <h1 style={myStyle}>Our Impact</h1>
       <p>See how we're making a difference.</p>
     </header>
   );
 }
 
 function Footer() {
+  const footerStyle = {
+    color: "white",
+    backgroundColor: "Green",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  }
+
   return (
     <footer>
-      <p>Join us in making a difference!</p>
+      <p style={footerStyle}>Join us in making a difference!</p>
     </footer>
   );
 }
