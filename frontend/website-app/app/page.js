57337e4068a5b333b6103419a77b7adcfef0f7ce
JayHall2002 <Ahallaj20002@gmail.com>
2024-10-22 13:35:57 -0500

    Redesigned Home Page, About Us Page, Donations Page, Contact Us Page

--- frontend/website-app/app/page.js
+++ frontend/website-app/app/page.js
@@ -1,88 +1,156 @@
-import Navbar from "../../website-app/app/components/navbar";
+"use client";
+import "./styles.css";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import ourmission from "../app/images/landscaping.jpeg";
+import landScapeImage from "../app/images/landscaping.jpeg";
+import Navbar from "../app/components/navbar";
+import Donationbutton from "./components/donate";
+import Learnmorebutton from "./components/learnmore";
+import Link from "next/link";
+import scapingprogram from "../app/images/scapingprogram.jpeg";
+import youthprogram from "../app/images/youthprogram.jpeg";
+import placeprogram from "../app/images/outreachprogram.jpeg";
+import partnershipprogram from "../app/images/partnershipprogram.jpeg";
+import getinvolved from "../app/images/joinus.jpeg";
+
 
 export default function Home() {
+    const [isMounted, setIsMounted] = useState(false);
+    const [typedText, setTypedText] = useState("");
+    const [isHoveringMission, setIsHoveringMission] = useState(false);
+    const [isHoveringPrograms, setIsHoveringPrograms] = useState(false);
+    const [isHoveringLearnMore, setIsHoveringLearnMore] = useState(false);
+    const [isShowingLandScapeImage, setIsShowingLandScapeImage] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    const typeText = () => {
+        const description = "Amplify the voice of the Latino Communities in their pursuit of social and environmental justice through outreach, education and advocacy across the Chesapeake Bay region.";
+        const typingSpeed = 1000 / 60;
+        let index = 0;
+        setTypedText("");
+
+        const type = () => {
+            if (index <= description.length) {
+                setTypedText(description.slice(0, index));
+                index++;
+                if (index <= description.length) {
+                    setTimeout(type, typingSpeed);
+                }
+            }
+        };
+
+        type();
+    };
+
+    const programButtons = ["Equitable Landscaping Training Program", "Youth Environmental Programs", "Place-Based Outreach and Restoration Programs", "Partnership Programs"," Get Involved / Join Us(Calendar)"];
+
     return (
         <>
-            <Navbar />
-            {/* Main container with padding */}
-            <div style={{ padding: "20px" }}>
+            <div className={isMounted ? "fade-in" : ""} style={{ padding: "20px" }}>
+                <Donationbutton style={{ marginBottom: "20px" , }} />
+                <Navbar />
                 
-                {/* Facebook Page Embed - This section centers the Facebook page embed in the middle of the page */}
-                <div style={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}>
-                    <iframe
-                        // The Facebook page plugin is embedded using an iframe
-                        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FEcoLatinos&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-                        width="500" // Sets the width of the iframe
-                        height="600" // Sets the height of the iframe
-                        style={{ border: "none", overflow: "hidden" }} // Ensures no borders or overflow within the iframe
-                        scrolling="no" // Disables scrolling inside the iframe
-                        frameBorder="0" // Removes the border from the iframe
-                        allowFullScreen={true} // Allows the iframe to be viewed in fullscreen mode
-                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" // Permissions for the iframe content
-                    ></iframe>
+                {isMounted && (
+                    <div className="fade-in flex justify-start space-x-16 italic" style={{ marginTop: "20px", opacity: 0, animation: "fadeIn 9s ease-in forwards", animationDelay: "3s" }}>
+                        <h1 className="text-4xl font-mono font-semibold text-gray-600">Our Mission</h1>  
+                    </div>
+                        
+                )}
+                {isMounted && (
+                    <div className="fade-in flex flex-row justify-start space-x-16" style={{ marginTop: "20px", opacity: 0, animation: "fadeIn 9s ease-in forwards", animationDelay: "6s" }}>
+                        <div className="flex flex-col">
+                            <h2 className="text-2xl font-mono font-semibold tracking-widest leading-loose text-green-600 text-left">Amplify the voice of the Latino Communities in their pursuit of social and environmental justice through outreach, education and advocacy across the Chesapeake Bay region.</h2>
+                            <Link href="/aboutus" passHref>  
+                                <div className="flex justify-center" style={{ marginTop: "20px" }}>
+                                    <Learnmorebutton />
+                                </div>
+                            </Link>
+                        </div>
+                        <Image src={ourmission} alt="our mission" height={300} width={600} className="text-blue-500 font-mono rounded-full shadow-lg border-2" style={{ marginBottom: "20px" }} />
+                    </div>
+                )}
+                {isMounted && (
+                    <div className="fade-in" style={{ marginTop: "40px", opacity: 0, animation: "fadeIn 9s ease-in forwards", animationDelay: "9s" }}>
+                        <div className="fade-in flex flex-row justify-start space-x-16 italic">
+                            <h1 className="text-4xl font-mono font-semibold text-gray-600">Our Programs</h1> 
+                        </div>
+                        <div className="flex justify-start space-x-16 image-container" style={{ marginTop: "20px" , marginBottom: "100px"}}>
+                            <div className="flex flex-col items-center group relative">
+                                <Image src={scapingprogram} alt="Land Scaping Program" height={200} width={200} className="text-blue-500 font-mono rounded-full shadow-lg border-2 hover:scale-125 hover:blur-sm duration-500" />
+                                <p className="mt-2 text-center font-mono text-gray-600">Land Scaping Program</p>
+                                <Link href="/landscaping" passHref>
+                                    <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 mt-2 px-4 py-2 bg-transparent border-2 border-white text-white font-bold rounded-full shadow-lg hover:bg-green-700">
+                                        Learn More
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="flex flex-col items-center group relative">
+                                <Image src={youthprogram} alt="Youth Program" height={200} width={200} className="text-blue-500 font-mono rounded-full shadow-lg border-2 hover:scale-125 hover:blur-sm duration-500" />
+                                <p className="mt-2 text-center font-mono text-gray-600">Youth Program</p>
+                                <Link href="/landscaping" passHref>
+                                    <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 mt-2 px-4 py-2 bg-transparent border-2 border-white text-white font-bold rounded-full shadow-lg hover:bg-green-700">
+                                        Learn More
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="flex flex-col items-center group relative">
+                                <Image src={placeprogram} alt="Outreach Program" height={200} width={200} className="text-blue-500 font-mono rounded-full shadow-lg border-2 hover:scale-125 hover:blur-sm duration-500" />
+                                <p className="mt-2 text-center font-mono text-gray-600">Outreach Program</p>
+                                <Link href="/landscaping" passHref>
+                                    <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 mt-2 px-4 py-2 bg-transparent border-2 border-white text-white font-bold rounded-full shadow-lg hover:bg-green-700">
+                                        Learn More
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="flex flex-col items-center group relative">
+                                <Image src={partnershipprogram} alt="Partnership Program" height={200} width={200} className="text-blue-500 font-mono rounded-full shadow-lg border-2 hover:scale-125 hover:blur-sm duration-500" />
+                                <p className="mt-2 text-center font-mono text-gray-600">Partnership Program</p>
+                                <Link href="/landscaping" passHref>
+                                    <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 mt-2 px-4 py-2 bg-transparent border-2 border-white text-white font-bold rounded-full shadow-lg hover:bg-green-700">
+                                        Learn More
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className="flex flex-col items-center group relative">
+                                <Image src={getinvolved} alt="Join Us" height={200} width={200} className="text-blue-500 font-mono rounded-full shadow-lg border-2 hover:scale-125 hover:blur-sm duration-500" />
+                                <p className="mt-2 text-center font-mono text-gray-600">Join Us</p>
+                                <Link href="/landscaping" passHref>
+                                    <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 mt-2 px-4 py-2 bg-transparent border-2 border-white text-white font-bold rounded-full shadow-lg hover:bg-green-700">
+                                        Learn More
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                        
+                )}
+                {/* Subscription Text Entry Box */}
+                <div className="flex flex-col items-center" style={{ marginTop: "40px",marginBottom: "100px", animation: "fadeIn 9s ease-in forwards", animationDelay: "9s" }}>
+                    <h2 className="text-2xl font-bold mb-4">Subscribe to our Newsletter</h2>
+                    <input type="email" placeholder="Enter your email" className="border border-gray-300 rounded-md px-4 py-2 mb-4" />
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">Subscribe</button>
                 </div>
+                {/* Footer */}
 
-                {/* Section for welcome text with centered alignment */}
-                <div style={{ padding: "40px", textAlign: "center" }}>
-                    {/* Welcome message */}
-                    <h2 style={{ color: "#3ea23e" }}>Welcome to EcoLatinos</h2>
-                    {/* Short description of the organization's mission */}
-                    <p>We are dedicated to promoting environmental and social justice, especially within Spanish-speaking and under-resourced communities.</p>
-
-                    {/* Input textbox for user's name */}
-                    <input
-                        type="text" // Input type set to text
-                        placeholder="Enter your name" // Placeholder text guiding user input
-                        style={{
-                            width: "100%", // Full width of the container
-                            maxWidth: "600px", // Limits the maximum width
-                            padding: "10px", // Padding inside the textbox
-                            marginBottom: "20px", // Space below the textbox
-                            border: "2px solid #3ea23e", // Green border style
-                            borderRadius: "5px" // Rounded corners for the textbox
-                        }}
-                    />
-                    <br /> {/* Line break for spacing */}
-
-                    {/* Input textbox for user's email */}
-                    <input
-                        type="email" // Input type set to email
-                        placeholder="Enter your email" // Placeholder text guiding user input
-                        style={{
-                            width: "100%", // Full width of the container
-                            maxWidth: "600px", // Limits the maximum width
-                            padding: "10px", // Padding inside the textbox
-                            marginBottom: "20px", // Space below the textbox
-                            border: "2px solid #3ea23e", // Green border style
-                            borderRadius: "5px" // Rounded corners for the textbox
-                        }}
-                    />
-                    <br /> {/* Line break for spacing */}
-
-                    {/* Textarea for user's message input */}
-                    <textarea
-                        placeholder="Enter your message" // Placeholder text guiding user input
-                        rows="4" // Sets the number of visible text lines
-                        style={{
-                            width: "100%", // Full width of the container
-                            maxWidth: "600px", // Limits the maximum width
-                            padding: "10px", // Padding inside the textbox
-                            border: "2px solid #3ea23e", // Green border style
-                            borderRadius: "5px" // Rounded corners for the textbox
-                        }}
-                    />
-                </div>
-
-                {/* Footer section at the bottom of the page */}
                 <footer style={{
-                    backgroundColor: "#3ea23e", // Green background color
-                    color: "white", // White text color
-                    textAlign: "center", // Centered text
-                    padding: "20px" // Padding inside the footer
+                    position: "fixed", 
+                    left: 0, 
+                    bottom: 0, 
+                    width: "100%", 
+                    backgroundColor: "#3ea23e", 
+                    color: "white", 
+                    textAlign: "center", 
+                    padding: "20px" 
                 }}>
-                    <p>© 2024 EcoLatinos - All Rights Reserved</p> {/* Copyright notice */}
+                    <p>© 2024 EcoLatinos - All Rights Reserved</p>
                 </footer>
             </div>
         </>
     );
 }
+
+
